43ba47eb09fc664f8fc1debf92b3fb0f0f62f69d
Narendra Lama <blazeraze.naren@gmail.com>
2024-03-16 18:13:13 +0200

    fixed the routing path and filename; removed unused static routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import { Ships } from "./ships/ships";
+import { ShipDetail } from "./shipDetail/ship-detail";
 
-export const App = () => {
-  return (
-    <>
-      <Ships />
-      <Outlet />
-    </>
-  );
-};
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Ships />
+        {/* Outlet needs to be inside root path to work*/}
+        <Outlet />
+      </>
+    ),
+    children: [
+      {
+        path: "view/:shipId",
+        element: <ShipDetail />,
+      },
+    ],
+  },
+]);
+
+export default () => <RouterProvider router={router} />;
