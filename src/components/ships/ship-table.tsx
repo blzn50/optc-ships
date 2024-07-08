386d37c5598c828ec8b4769bfeb7aca3fdfdc888
Narendra Lama <blazeraze.naren@gmail.com>
2024-07-08 21:30:40 +0300

    cola and super cola count column's visibility is toggleable

--- src/components/ships/ship-table.tsx
+++ src/components/ships/ship-table.tsx
@@ -3,6 +3,7 @@ import {
   type ColumnDef,
   type SortingState,
   type FilterFn,
+  type VisibilityState,
   flexRender,
   getCoreRowModel,
   getFilteredRowModel,
@@ -15,6 +16,12 @@ import "lazysizes";
 import { ArrowDown, ArrowUp, ArrowUpDown } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import {
   Table,
   TableBody,
@@ -29,7 +36,7 @@ import {
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "../ui/select";
+} from "@/components/ui/select";
 import { searchVal } from "@/searchStore";
 
 interface ShipTableProps<TData, TValue> {
@@ -60,6 +67,7 @@ export function ShipTable<TData, TValue>({
       desc: false,
     },
   ]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
 
   const $searchVal = useStore(searchVal);
 
@@ -73,9 +81,11 @@ export function ShipTable<TData, TValue>({
       sorting,
       pagination,
       globalFilter: $searchVal,
+      columnVisibility,
     },
     globalFilterFn: fuzzyFilter,
     enableSortingRemoval: false,
+    onColumnVisibilityChange: setColumnVisibility,
     getCoreRowModel: getCoreRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
     getPaginationRowModel: getPaginationRowModel(),
@@ -87,6 +97,44 @@ export function ShipTable<TData, TValue>({
 
   return (
     <div>
+      <div className="flex my-3">
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto" size="sm">
+              Toggle columns
+              <svg
+                width="15"
+                height="15"
+                viewBox="0 0 15 15"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                className="ml-2 h-4 w-4"
+              >
+                <path
+                  d="M3.13523 6.15803C3.3241 5.95657 3.64052 5.94637 3.84197 6.13523L7.5 9.56464L11.158 6.13523C11.3595 5.94637 11.6759 5.95657 11.8648 6.15803C12.0536 6.35949 12.0434 6.67591 11.842 6.86477L7.84197 10.6148C7.64964 10.7951 7.35036 10.7951 7.15803 10.6148L3.15803 6.86477C2.95657 6.67591 2.94637 6.35949 3.13523 6.15803Z"
+                  fill="currentColor"
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((colToFilter) => colToFilter.getCanHide())
+              .map((col) => (
+                <DropdownMenuCheckboxItem
+                  key={col.id}
+                  checked={col.getIsVisible()}
+                  onCheckedChange={(value) => col.toggleVisibility(!!value)}
+                >
+                  {col.columnDef.meta?.displayLabel}
+                </DropdownMenuCheckboxItem>
+              ))}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
       <div className="rounded-md border">
         <Table>
           <TableHeader>
