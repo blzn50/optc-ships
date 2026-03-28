de7a0ae25322c7be18a82d2b63ede0359a408168
Narendra Lama <blazeraze.naren@gmail.com>
2026-03-28 22:01:33 +0200

    chore: toast msg update

--- src/components/ships/ships.tsx
+++ src/components/ships/ships.tsx
@@ -15,8 +15,8 @@ export function Ships() {
     if (dbVersion < DB_VERSION) {
       setTimeout(() => {
         toast({
-          title: "Update: 14 Aug 2025",
-          description: "Jewelry Margherita ship added",
+          title: "Update: 28 Mar 2026",
+          description: "Great Erik ship added",
         });
       }, 500);
       // update local storage
--- src/data/version.ts
+++ src/data/version.ts
@@ -1,1 +1,1 @@
-export const DB_VERSION = 7;
+export const DB_VERSION = 8;
