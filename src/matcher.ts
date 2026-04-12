b421b2515ce56bdc3fd35ae27bdaa27dd2106c43
Narendra Lama <blazeraze.naren@gmail.com>
2026-04-12 16:20:22 +0300

    feat: filter matcher function add to handle various filter cases; beneficial effect cases add in 1st phase

---
+++ src/matcher.ts
@@ -0,0 +1,42 @@
+import type { EffectUnion } from "./types/Filter";
+
+type FilterMatcher = {
+  textMatcher: string;
+  regexMatcher: RegExp;
+};
+
+export const filterMatcher = (
+  type: EffectUnion | null,
+  turnCount: number | null,
+): FilterMatcher => {
+  switch (type) {
+    case "atk":
+      return {
+        textMatcher: `boosts atk by 1x`,
+        regexMatcher:
+          /boosts\s+(?:([\[\]\w\s.,'’-]*(?:and\s+[\[\]\w\s.,'’-]+)?\s+)?)?atk\s+((.+?\s+)?)by\s+(?:approximately\s+)?(\d+(\.\d+)?)x/i,
+      };
+    case "hp":
+      return {
+        textMatcher: `boosts hp by 1x`,
+        regexMatcher:
+          /boosts\s+(?:([\[\]\w\s.,'’-]*(?:and\s+[\[\]\w\s.,'’-]+)?\s+)?)?hp\s+by\s+(?:approximately\s+)?(\d+(?:\.\d+)?)x?/i,
+      };
+    case "land perfect strikes":
+      return {
+        textMatcher: "land perfect strikes",
+        regexMatcher: /land perfect strikes/i,
+      };
+    case "reduce special charge":
+      return {
+        textMatcher: `reduces special charge time by ${turnCount} turn`,
+        regexMatcher:
+          /reduces\s+(?:([\w\s'’,]*(?:and\s+[\w\s'’,]+)?\s+)?)?special\s+charge\s+time\s+by\s+(\d+)\s+turns?/i,
+      };
+    default:
+      return {
+        textMatcher: "",
+        regexMatcher: /\w/i,
+      };
+  }
+};
